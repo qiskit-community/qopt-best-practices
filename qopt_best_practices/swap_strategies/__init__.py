7da43ec7edf1d19743ffed660049495d9528f047
Elena Peña Tapia <epenatap@gmail.com>
2023-08-31 15:22:00 +0200

    Restructure

---
+++ qopt_best_practices/swap_strategies/__init__.py
@@ -0,0 +1,8 @@
+"""SWAP strategies"""
+
+from .circuit_utils import create_qaoa_swap_circuit, make_meas_map
+
+__all__ = [
+    "create_qaoa_swap_circuit",
+    "make_meas_map"
+]
--- qopt_best_practices/utils/__init__.py
+++ qopt_best_practices/utils/__init__.py
@@ -1,10 +1,9 @@
 """Utils"""
 
-from .circuit_utils import create_qaoa_swap_circuit
+
 from .graph_utils import build_graph, build_paulis
 
 __all__ = [
-    "create_qaoa_swap_circuit",
     "build_graph",
     "build_paulis"
 ]

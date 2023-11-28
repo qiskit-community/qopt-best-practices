95db17689d15e18a36894f6e5373bee4ba2ec04d
Daniel Egger <deg@zurich.ibm.com>
2023-11-28 12:50:10 +0100

    * Added support for weighted graphs.

--- qopt_best_practices/utils/graph_utils.py
+++ qopt_best_practices/utils/graph_utils.py
@@ -14,22 +14,27 @@ def build_max_cut_graph(paulis: list[tuple[str, float]]) -> nx.Graph:
     Returns:
         A networkx graph.
     """
-    edges = []
-    for pauli_str, _ in paulis:
-        edges.append([idx for idx, char in enumerate(pauli_str[::-1]) if char == "Z"])
+    graph = nx.Graph()
 
-    return nx.from_edgelist(edges)
+    for pauli_str, weight in paulis:
+        edge = [idx for idx, char in enumerate(pauli_str[::-1]) if char == "Z"]
+        graph.add_edge(edge[0], edge[1], weight=weight)
+
+    return graph
 
 
 def build_max_cut_paulis(graph: nx.Graph) -> list[tuple[str, float]]:
     """Convert the graph to Pauli list.
 
-    This function does the inverse of `build_graph`
+    This function does the inverse of `build_max_cut_graph`
     """
     pauli_list = []
     for edge in graph.edges():
         paulis = ["I"] * len(graph)
         paulis[edge[0]], paulis[edge[1]] = "Z", "Z"
-        pauli_list.append(("".join(paulis)[::-1], 1.0))
+
+        weight = graph.get_edge_data(edge[0], edge[1]).get("weight", 1.0)
+
+        pauli_list.append(("".join(paulis)[::-1], weight))
 
     return pauli_list

59baedf776a9025907a93b79c4eb123f4a5cae65
Elena Peña Tapia <epenatap@gmail.com>
2024-02-20 21:03:06 +0100

    Update code to support backend v1 and v2, add tests. Improve notebook plotting function.

--- qopt_best_practices/qubit_selection/metric_evaluators.py
+++ qopt_best_practices/qubit_selection/metric_evaluators.py
@@ -17,20 +17,35 @@ def evaluate_fidelity(path: list[int], backend: Backend, edges: rx.EdgeList) -> 
     """
 
     two_qubit_fidelity = {}
-    target = backend.target
 
-    try:
-        gate_name = list(set(TWO_Q_GATES).intersection(backend.operation_names))[0]
-    except IndexError as exc:
-        raise ValueError("Could not identify two-qubit gate") from exc
-
-    for edge in edges:
+    if backend.version == 2:
+        target = backend.target
         try:
-            cx_error = target[gate_name][edge].error
-        except:  # pylint: disable=bare-except
-            cx_error = target[gate_name][edge[::-1]].error
+            gate_name = list(set(TWO_Q_GATES).intersection(backend.operation_names))[0]
+        except IndexError as exc:
+            raise ValueError("Could not identify two-qubit gate") from exc
 
-        two_qubit_fidelity[tuple(edge)] = 1 - cx_error
+        for edge in edges:
+            try:
+                cx_error = target[gate_name][edge].error
+            except:  # pylint: disable=bare-except
+                cx_error = target[gate_name][edge[::-1]].error
+
+            two_qubit_fidelity[tuple(edge)] = 1 - cx_error
+    else:
+        props = backend.properties()
+        try:
+            gate_name = list(set(TWO_Q_GATES).intersection(backend.configuration().basis_gates))[0]
+        except IndexError as exc:
+            raise ValueError("Could not identify two-qubit gate") from exc
+
+        for edge in edges:
+            try:
+                cx_error = props.gate_error(gate_name, edge)
+            except:
+                cx_error = props.gate_error(gate_name, edge[::-1])
+
+            two_qubit_fidelity[tuple(edge)] = 1 - cx_error
 
     if not path or len(path) == 1:
         return 0.0
@@ -38,5 +53,4 @@ def evaluate_fidelity(path: list[int], backend: Backend, edges: rx.EdgeList) -> 
     fidelity = 1.0
     for idx in range(len(path) - 1):
         fidelity *= two_qubit_fidelity[(path[idx], path[idx + 1])]
-
     return fidelity

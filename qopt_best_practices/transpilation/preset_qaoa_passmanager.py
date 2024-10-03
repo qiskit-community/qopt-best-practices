da953404ec1de6f9439147b2ea32e0ef26a51856
Daniel Egger <deg@zurich.ibm.com>
2024-10-03 15:51:48 +0200

    * Adding transpilation draft

---
+++ qopt_best_practices/transpilation/preset_qaoa_passmanager.py
@@ -0,0 +1,47 @@
+"""Make a pass manager to transpile QAOA."""
+
+from typing import Any, Dict
+
+from qiskit.transpiler import PassManager
+from qiskit.transpiler.passes import HighLevelSynthesis, InverseCancellation
+from qiskit.transpiler.passes.routing.commuting_2q_gate_routing import (
+    FindCommutingPauliEvolutions,
+    Commuting2qGateRouter,
+)
+from qiskit.circuit.library import CXGate
+
+from qopt_best_practices.transpilation.qaoa_construction_pass import QAOAConstructionPass
+
+
+def qaoa_swap_strategy(config: Dict[str, Any]):
+    """Provide a pass manager to build the QAOA cirucit.
+
+    This function will be extended in the future.
+    """
+
+    num_layers = config.get("num_layers", 1)
+    swap_strategy = config.get("swap_strategy", None)
+    edge_coloring = config.get("edge_coloring", None)
+
+    if swap_strategy is None:
+        raise ValueError("No swap_strategy provided in config.")
+
+    if edge_coloring is None:
+        raise ValueError("No edge_coloring provided in config.")
+
+    # 2. define pass manager for cost layer
+    qaoa_pm = PassManager(
+        [
+            HighLevelSynthesis(basis_gates=["PauliEvolution"]),
+            FindCommutingPauliEvolutions(),
+            Commuting2qGateRouter(
+                swap_strategy,
+                edge_coloring,
+            ),
+            HighLevelSynthesis(basis_gates=["x", "cx", "sx", "rz", "id"]),
+            InverseCancellation(gates_to_cancel=[CXGate()]),
+            QAOAConstructionPass(num_layers),
+        ]
+    )
+
+    return qaoa_pm
---
+++ test/test_qaoa_construction.py
@@ -0,0 +1,83 @@
+"""Test the construction of the QAOA ansatz."""
+
+
+from unittest import TestCase
+
+from qiskit import QuantumCircuit, transpile
+from qiskit.circuit import Parameter
+from qiskit.circuit.library import QAOAAnsatz
+from qiskit.primitives import StatevectorEstimator
+from qiskit.quantum_info import SparsePauliOp
+from qiskit.transpiler import PassManager
+from qiskit.transpiler.passes.routing.commuting_2q_gate_routing import SwapStrategy
+
+from qopt_best_practices.transpilation.qaoa_construction_pass import QAOAConstructionPass
+from qopt_best_practices.transpilation.preset_qaoa_passmanager import qaoa_swap_strategy
+
+class TestQAOAConstruction(TestCase):
+    """Test the construction of the QAOA ansatz."""
+
+    def setUp(self):
+        """Set up re-used variables."""
+        gamma = Parameter("γ")
+        cost_op = QuantumCircuit(4)
+        cost_op.rzz(2 * gamma, 0, 1)
+        cost_op.rzz(2 * gamma, 2, 3)
+        cost_op.swap(0, 1)
+        cost_op.swap(2, 3)
+        cost_op.rzz(2 * gamma, 1, 2)
+
+        self.cost_op_circ = transpile(cost_op, basis_gates = ["sx", "cx", "x", "rz"])
+
+        self.cost_op = SparsePauliOp.from_list([("IIZZ", 1), ("ZZII", 1), ("ZIIZ", 1)])
+
+        self.config = {
+            "swap_strategy": SwapStrategy.from_line(list(range(4))),
+            "edge_coloring": {(idx, idx + 1): (idx + 1) % 2 for idx in range(4)},
+        }
+
+
+    def test_depth_one(self):
+        """Compare the pass with the SWAPs and ensure the measurements are ordered properly."""
+
+        qaoa_pm = qaoa_swap_strategy(self.config)
+
+        cost_op_circ = QAOAAnsatz(
+            self.cost_op, 
+            initial_state=QuantumCircuit(4),
+            mixer_operator=QuantumCircuit(4)
+        ).decompose(reps=1)
+
+        print(cost_op_circ)
+
+        def pm_callback(**kwargs):
+            print("Callback from test_depth_one:", kwargs["property_set"])
+
+        ansatz = qaoa_pm.run(cost_op_circ, callback=pm_callback)
+
+        print(qaoa_pm.property_set)
+        #print(qaoa_pm.passes)
+
+        print(ansatz)
+
+    def test_depth_two_qaoa_pass(self):
+        """Compare the pass with the SWAPs to an all-to-all construction.
+        
+        Note: this test only works as is because p is even and we don't have the previous
+        passes to give us the qubit permutations.
+        """
+        qaoa_pm = PassManager([QAOAConstructionPass(num_layers=2)])
+
+        ansatz = qaoa_pm.run(self.cost_op_circ)
+        ansatz.remove_final_measurements(inplace=True)
+
+        estimator = StatevectorEstimator()
+
+        value = estimator.run([(ansatz, self.cost_op, [1, 2, 3, 4])]).result()[0].data.evs
+        
+        library_ansatz = QAOAAnsatz(self.cost_op, reps=2)
+        library_ansatz = transpile(library_ansatz, basis_gates = ["cx", "rz", "rx", "h"])
+
+        expected = estimator.run([(library_ansatz, self.cost_op, [1, 2, 3, 4])]).result()[0].data.evs
+
+        self.assertAlmostEqual(value, expected)

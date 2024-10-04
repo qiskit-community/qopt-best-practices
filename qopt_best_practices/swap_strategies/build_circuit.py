c1576a564001fb2b7b56cd2bcf020eb93e52eaf9
Daniel Egger <deg@zurich.ibm.com>
2024-10-04 18:45:28 +0200

    * Lint

--- qopt_best_practices/swap_strategies/build_circuit.py
+++ qopt_best_practices/swap_strategies/build_circuit.py
@@ -116,7 +116,7 @@ def apply_qaoa_layers(  # pylint: disable=too-many-arguments,too-many-locals,too
     return new_circuit
 
 
-def create_qaoa_swap_circuit(  # pylint: disable=too-many-arguments
+def create_qaoa_swap_circuit(  # pylint: disable=too-many-arguments,too-many-positional-arguments
     cost_operator: SparsePauliOp,
     swap_strategy: SwapStrategy,
     edge_coloring: dict = None,

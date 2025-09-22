9b150130613102f6ddb104535d0cf43e3d1c2786
ElePT <epenatap@gmail.com>
2025-09-22 10:20:37 +0200

    Add annotated qaoa ansatz, annotated transpiler passes, tests. How-to will come soon.

---
+++ test/test_annotated_qaoa_ansatz.py
@@ -0,0 +1,59 @@
+import unittest
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import SparsePauliOp
+from qopt_best_practices.utils import build_max_cut_paulis
+from networkx import barabasi_albert_graph
+from qopt_best_practices.circuit_library import annotated_qaoa_ansatz
+
+
+class TestAnnotatedQAOAAnsatz(unittest.TestCase):
+    def setUp(self):
+        graph = barabasi_albert_graph(n=4, m=2, seed=42)
+        paulis = build_max_cut_paulis(graph)
+        self.hamiltonian = SparsePauliOp.from_list(paulis)
+
+    def test_returns_quantum_circuit(self):
+        """Test that the function returns a quantum circuit."""
+        circuit = annotated_qaoa_ansatz(self.hamiltonian, reps=1)
+        self.assertIsInstance(circuit, QuantumCircuit)
+
+    def test_correct_number_of_qubits(self):
+        """Test that the number of qubits matches the Hamiltonian."""
+        circuit = annotated_qaoa_ansatz(self.hamiltonian, reps=1)
+        self.assertEqual(circuit.num_qubits, self.hamiltonian.num_qubits)
+
+    def test_box_structure(self):
+        """Test that a 1 layer qaoa contains init_state, cost_layer and mixer boxes."""
+        circuit = annotated_qaoa_ansatz(self.hamiltonian, reps=1)
+        gate_names = [instr.operation.name for instr in circuit.data]
+        self.assertEqual(gate_names, ["box", "box", "box"])
+        for i, instr in enumerate(circuit.data):
+            self.assertEqual(instr.operation.name, "box")
+            if i == 0:
+                self.assertEqual(instr.operation.annotations[0].namespace, "qaoa.init_state")
+            elif i == 1:
+                self.assertEqual(instr.operation.annotations[0].namespace, "qaoa.cost_layer")
+            else:
+                self.assertEqual(instr.operation.annotations[0].namespace, "qaoa.mixer")
+
+    def test_zero_layers(self):
+        """Test that 0 layers equals initial state box."""
+        circuit = annotated_qaoa_ansatz(self.hamiltonian, reps=0)
+        self.assertEqual(len(circuit.data), 1)
+
+    def test_multiple_layers(self):
+        """Test that 3 layers equals 3*2+1=7 boxes."""
+        circuit = annotated_qaoa_ansatz(self.hamiltonian, reps=3)
+        self.assertGreaterEqual(len(circuit.data), 7)
+        for i, instr in enumerate(circuit.data):
+            self.assertEqual(instr.operation.name, "box")
+            if i < 3:
+                self.assertEqual(instr.operation.annotations[0].payload, "1")
+            elif i >= 3 and i < 5:
+                self.assertEqual(instr.operation.annotations[0].payload, "2")
+            else:
+                self.assertEqual(instr.operation.annotations[0].payload, "3")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test/test_annotated_transpilation.py
@@ -0,0 +1,277 @@
+import unittest
+import networkx as nx
+from networkx import barabasi_albert_graph
+
+from qiskit.primitives import StatevectorEstimator
+from qiskit.quantum_info import SparsePauliOp
+from qiskit.transpiler import Layout, PassManager
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+from qiskit.transpiler.passes.routing.commuting_2q_gate_routing import (
+    SwapStrategy,
+    Commuting2qGateRouter,
+)
+from qiskit.circuit.library import CXGate
+from qiskit.transpiler.passes import HighLevelSynthesis, InverseCancellation
+from qiskit_optimization.applications import Maxcut
+from qiskit.providers.fake_provider import GenericBackendV2
+
+from qopt_best_practices.circuit_library import annotated_qaoa_ansatz
+from qopt_best_practices.utils import build_max_cut_paulis
+from qopt_best_practices.transpilation.cost_layer import get_cost_layer
+from qopt_best_practices.transpilation.prepare_cost_layer import PrepareCostLayer
+from qopt_best_practices.transpilation.swap_cancellation_pass import SwapToFinalMapping
+from qopt_best_practices.transpilation.qaoa_construction_pass import QAOAConstructionPass
+from qopt_best_practices.qubit_selection import BackendEvaluator
+from qopt_best_practices.transpilation import (
+    AnnotatedPrepareCostLayer,
+    AnnotatedCommuting2qGateRouter,
+    AnnotatedSwapToFinalMapping,
+    SynthesizeAndSimplifyCostLayer,
+    UnrollBoxes,
+)
+
+
+def get_problem_barabasi(n=4, m=3):
+    graph = barabasi_albert_graph(n=n, m=m, seed=42)
+    local_correlators = build_max_cut_paulis(graph)
+    cost_operator = SparsePauliOp.from_list(local_correlators)
+    cost_layer = get_cost_layer(cost_operator)
+    return cost_operator, cost_layer, graph
+
+
+def get_problem_maxcut(n=4, elist=None):
+    if elist is None:
+        elist = [(0, 1, 1.0), (0, 2, 1.0), (1, 2, 1.0), (2, 3, 1.0)]
+    graph = nx.Graph()
+    graph.add_nodes_from(range(n))
+    graph.add_weighted_edges_from(elist)
+    max_cut = Maxcut(graph)
+    qubo = max_cut.to_quadratic_program()
+    cost_operator, _ = qubo.to_ising()
+    cost_layer = get_cost_layer(cost_operator)
+    return cost_operator, cost_layer, graph
+
+
+def backend_and_layout_a(cost_layer):
+    backend = GenericBackendV2(
+        num_qubits=cost_layer.num_qubits,
+        basis_gates=["x", "sx", "cz", "id", "rz"],
+        seed=0,
+    )
+    path_finder = BackendEvaluator(backend)
+    path, _, _ = path_finder.evaluate(cost_layer.num_qubits)
+    initial_layout = Layout.from_intlist(path, cost_layer.qregs[0])
+    return backend, initial_layout
+
+
+class TestAnnotatedTranspilation(unittest.TestCase):
+    def setUp(self):
+        self.estimator = StatevectorEstimator()
+        self.test_cases = [
+            (1, 4, 2, [(0, 1, 1.0), (0, 2, 1.0), (1, 2, 1.0), (2, 3, 1.0)]),
+            (2, 4, 2, [(0, 1, 1.0), (0, 2, 1.0), (1, 2, 1.0), (2, 3, 1.0)]),
+            (3, 4, 2, [(0, 1, 1.0), (0, 2, 1.0), (1, 2, 1.0), (2, 3, 1.0)]),
+            (2, 5, 3, [(0, 1, 1.0), (0, 2, 1.0), (1, 2, 1.0), (2, 3, 1.0), (3, 4, 1.0)]),
+            (
+                2,
+                7,
+                3,
+                [
+                    (0, 1, 1.0),
+                    (0, 2, 1.0),
+                    (1, 2, 1.0),
+                    (2, 3, 1.0),
+                    (3, 1, 1.0),
+                    (3, 4, 1.0),
+                    (3, 5, 1.0),
+                    (4, 6, 1.0),
+                ],
+            ),
+        ]
+
+    def qopt_vs_annot_basic(
+        self,
+        cost_layer,
+        hamiltonian,
+        num_qaoa_layers,
+        backend,
+        initial_layout,
+        estimator,
+        param_values,
+    ):
+        swap_strategy = SwapStrategy.from_line(list(range(cost_layer.num_qubits)))
+        edge_coloring = {(i, i + 1): (i + 1) % 2 for i in range(cost_layer.num_qubits)}
+
+        # QOpt pipeline
+        pre_init_qopt = PassManager(
+            [
+                PrepareCostLayer(),
+                Commuting2qGateRouter(swap_strategy, edge_coloring),
+                SwapToFinalMapping(),
+                QAOAConstructionPass(num_layers=num_qaoa_layers),
+            ]
+        )
+        qopt_pm = generate_preset_pass_manager(
+            backend=backend, optimization_level=3, initial_layout=initial_layout
+        )
+        qopt_pm.pre_init = pre_init_qopt
+        qopt_transpiled = qopt_pm.run(cost_layer)
+        isa_hamiltonian_qopt = hamiltonian.apply_layout(qopt_transpiled.layout)
+        qopt_transpiled.remove_final_measurements()
+        eval_qopt = (
+            estimator.run([(qopt_transpiled, isa_hamiltonian_qopt, param_values)])
+            .result()[0]
+            .data.values()
+        )
+
+        # Annotated pipeline
+        annotated_ansatz = annotated_qaoa_ansatz(hamiltonian, reps=num_qaoa_layers)
+        pre_init_annot = PassManager(
+            [
+                AnnotatedPrepareCostLayer(),
+                AnnotatedCommuting2qGateRouter(swap_strategy, edge_coloring),
+                AnnotatedSwapToFinalMapping(),
+                UnrollBoxes(),
+            ]
+        )
+        annot_pm = generate_preset_pass_manager(
+            backend=backend, optimization_level=3, initial_layout=initial_layout
+        )
+        annot_pm.pre_init = pre_init_annot
+        annot_transpiled = annot_pm.run(annotated_ansatz)
+        isa_hamiltonian_annot = hamiltonian.apply_layout(annot_transpiled.layout)
+        annot_transpiled.remove_final_measurements()
+        eval_annot = (
+            estimator.run([(annot_transpiled, isa_hamiltonian_annot, param_values)])
+            .result()[0]
+            .data.values()
+        )
+        print("EVALS:", list(eval_annot), list(eval_qopt))
+        self.assertEqual(list(eval_annot), list(eval_qopt))
+        for key in annot_transpiled.count_ops():
+            self.assertEqual(annot_transpiled.count_ops()[key], qopt_transpiled.count_ops()[key])
+
+    def run_basic_test(self, hamiltonian, cost_layer, num_qaoa_layers, backend, initial_layout):
+        optimal_gamma = [5.11350346] * num_qaoa_layers
+        optimal_beta = [5.52673212] * num_qaoa_layers
+        param_values = optimal_gamma + optimal_beta
+        self.qopt_vs_annot_basic(
+            cost_layer,
+            hamiltonian,
+            num_qaoa_layers,
+            backend,
+            initial_layout,
+            self.estimator,
+            param_values,
+        )
+
+    def qopt_vs_annot_optimized(
+        self,
+        cost_layer,
+        hamiltonian,
+        num_qaoa_layers,
+        backend,
+        initial_layout,
+        estimator,
+        param_values,
+    ):
+        swap_strategy = SwapStrategy.from_line(list(range(cost_layer.num_qubits)))
+        edge_coloring = {(i, i + 1): (i + 1) % 2 for i in range(cost_layer.num_qubits)}
+
+        # QOpt pipeline
+        pre_init_qopt = PassManager(
+            [
+                PrepareCostLayer(),
+                Commuting2qGateRouter(swap_strategy, edge_coloring),
+                SwapToFinalMapping(),
+                HighLevelSynthesis(basis_gates=["x", "cx", "sx", "rz", "id"]),
+                InverseCancellation(gates_to_cancel=[CXGate()]),
+                QAOAConstructionPass(num_layers=num_qaoa_layers),
+            ]
+        )
+        qopt_pm = generate_preset_pass_manager(
+            backend=backend, optimization_level=3, initial_layout=initial_layout
+        )
+        qopt_pm.pre_init = pre_init_qopt
+        qopt_transpiled = qopt_pm.run(cost_layer)
+        isa_hamiltonian_qopt = hamiltonian.apply_layout(qopt_transpiled.layout)
+        qopt_transpiled.remove_final_measurements()
+        eval_qopt = (
+            estimator.run([(qopt_transpiled, isa_hamiltonian_qopt, param_values)])
+            .result()[0]
+            .data.values()
+        )
+
+        # Annotated pipeline
+        annotated_ansatz = annotated_qaoa_ansatz(hamiltonian, reps=num_qaoa_layers)
+        pre_init_annot = PassManager(
+            [
+                AnnotatedPrepareCostLayer(),
+                AnnotatedCommuting2qGateRouter(swap_strategy, edge_coloring),
+                AnnotatedSwapToFinalMapping(),
+                SynthesizeAndSimplifyCostLayer(basis_gates=["x", "cx", "sx", "rz", "id"]),
+                UnrollBoxes(),
+            ]
+        )
+        annot_pm = generate_preset_pass_manager(
+            backend=backend, optimization_level=3, initial_layout=initial_layout
+        )
+        annot_pm.pre_init = pre_init_annot
+        annot_transpiled = annot_pm.run(annotated_ansatz)
+        isa_hamiltonian_annot = hamiltonian.apply_layout(annot_transpiled.layout)
+        annot_transpiled.remove_final_measurements()
+        eval_annot = (
+            estimator.run([(annot_transpiled, isa_hamiltonian_annot, param_values)])
+            .result()[0]
+            .data.values()
+        )
+        print("EVALS:", list(eval_annot), list(eval_qopt))
+        self.assertEqual(list(eval_annot), list(eval_qopt))
+        for key in annot_transpiled.count_ops():
+            self.assertEqual(annot_transpiled.count_ops()[key], qopt_transpiled.count_ops()[key])
+
+    def run_optimized_test(self, hamiltonian, cost_layer, num_qaoa_layers, backend, initial_layout):
+        optimal_gamma = [5.11350346] * num_qaoa_layers
+        optimal_beta = [5.52673212] * num_qaoa_layers
+        param_values = optimal_gamma + optimal_beta
+        self.qopt_vs_annot_optimized(
+            cost_layer,
+            hamiltonian,
+            num_qaoa_layers,
+            backend,
+            initial_layout,
+            self.estimator,
+            param_values,
+        )
+
+    def test_barabasi_albert_basic(self):
+        for layers, nodes, edges, _ in self.test_cases:
+            with self.subTest(layers=layers, nodes=nodes, edges=edges):
+                hamiltonian, cost_layer, _ = get_problem_barabasi(n=nodes, m=edges)
+                backend, initial_layout = backend_and_layout_a(cost_layer)
+                self.run_basic_test(hamiltonian, cost_layer, layers, backend, initial_layout)
+
+    def test_maxcut_basic(self):
+        for layers, nodes, edges, elist in self.test_cases:
+            with self.subTest(layers=layers, nodes=nodes, edges=edges):
+                hamiltonian, cost_layer, _ = get_problem_maxcut(n=nodes, elist=elist)
+                backend, initial_layout = backend_and_layout_a(cost_layer)
+                self.run_basic_test(hamiltonian, cost_layer, layers, backend, initial_layout)
+
+    def test_barabasi_albert_optimized(self):
+        for layers, nodes, edges, _ in self.test_cases:
+            with self.subTest(layers=layers, nodes=nodes, edges=edges):
+                hamiltonian, cost_layer, _ = get_problem_barabasi(n=nodes, m=edges)
+                backend, initial_layout = backend_and_layout_a(cost_layer)
+                self.run_optimized_test(hamiltonian, cost_layer, layers, backend, initial_layout)
+
+    def test_maxcut_optimized(self):
+        for layers, nodes, edges, elist in self.test_cases:
+            with self.subTest(layers=layers, nodes=nodes, edges=edges):
+                hamiltonian, cost_layer, _ = get_problem_maxcut(n=nodes, elist=elist)
+                backend, initial_layout = backend_and_layout_a(cost_layer)
+                self.run_optimized_test(hamiltonian, cost_layer, layers, backend, initial_layout)
+
+
+if __name__ == "__main__":
+    unittest.main()

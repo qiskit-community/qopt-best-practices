f3260ad2ace6d8f3dfe9bfdfbc5e436f0a77227e
ElePT <epenatap@gmail.com>
2025-10-06 10:11:56 +0200

    Add test against standard transpilation pipeline

--- test/test_annotated_transpilation.py
+++ test/test_annotated_transpilation.py
@@ -12,7 +12,7 @@ from qiskit.transpiler.passes.routing.commuting_2q_gate_routing import (
     SwapStrategy,
     Commuting2qGateRouter,
 )
-from qiskit.circuit.library import CXGate
+from qiskit.circuit.library import CXGate, qaoa_ansatz
 from qiskit.transpiler.passes import HighLevelSynthesis, InverseCancellation
 from qiskit.providers.fake_provider import GenericBackendV2
 
@@ -43,7 +43,6 @@ def get_problem_barabasi(n=4, m=3):
 
 def get_problem_maxcut(n=4, elist=None):
     """Get problem data for maxcut graph"""
-
     if elist is None:
         elist = [(0, 1, 1.0), (0, 2, 1.0), (1, 2, 1.0), (2, 3, 1.0)]
     graph = nx.Graph()
@@ -107,7 +106,7 @@ class TestAnnotatedTranspilation(unittest.TestCase):
         return list(result.data.values())
 
     def _assert_equivalence(self, expvals_1, expvals_2, circuit_1, circuit_2):
-        self.assertEqual(expvals_1, expvals_2)
+        self.assertAlmostEqual(float(expvals_1[0]), float(expvals_2[0]), places=12)
         for key in circuit_1.count_ops():
             self.assertEqual(circuit_1.count_ops()[key], circuit_2.count_ops()[key])
 
@@ -158,7 +157,42 @@ class TestAnnotatedTranspilation(unittest.TestCase):
 
         return qopt_transpiled, annot_transpiled
 
-    def _run_comparison(  # pylint: disable=too-many-positional-arguments
+    def _run_standard_and_annot(  # pylint: disable=too-many-positional-arguments
+        self, cost_layer, hamiltonian, num_qaoa_layers, backend, initial_layout, optimized=False
+    ):
+        """Run both the standard qaoa and new annotated pipeline"""
+
+        swap_strategy, edge_coloring = self._get_swap_strategy(cost_layer)
+
+        # Standard pipeline
+        standard_ansatz = qaoa_ansatz(hamiltonian, reps=num_qaoa_layers)
+        standard_pm = generate_preset_pass_manager(
+            backend=backend, optimization_level=3, initial_layout=initial_layout
+        )
+        standard_transpiled = standard_pm.run(standard_ansatz)
+
+        # Annotated pipeline
+        annotated_ansatz = annotated_qaoa_ansatz(hamiltonian, reps=num_qaoa_layers)
+        annot_passes = [
+            AnnotatedPrepareCostLayer(),
+            AnnotatedCommuting2qGateRouter(swap_strategy, edge_coloring),
+            AnnotatedSwapToFinalMapping(),
+        ]
+        if optimized:
+            annot_passes.append(
+                SynthesizeAndSimplifyCostLayer(basis_gates=["x", "cx", "sx", "rz", "id"])
+            )
+        annot_passes.append(UnrollBoxes())
+
+        annot_pm = generate_preset_pass_manager(
+            backend=backend, optimization_level=3, initial_layout=initial_layout
+        )
+        annot_pm.pre_init = PassManager(annot_passes)
+        annot_transpiled = annot_pm.run(annotated_ansatz)
+
+        return standard_transpiled, annot_transpiled
+
+    def _run_comparison_qopt(  # pylint: disable=too-many-positional-arguments
         self, hamiltonian, cost_layer, num_qaoa_layers, backend, initial_layout, optimized=False
     ):
         """Run both transpilation pipelines and compare expectation values
@@ -172,13 +206,28 @@ class TestAnnotatedTranspilation(unittest.TestCase):
         eval_annot = self._estimate(annot_transpiled, hamiltonian, param_values)
         self._assert_equivalence(eval_annot, eval_qopt, annot_transpiled, qopt_transpiled)
 
+    def _run_comparison_standard(  # pylint: disable=too-many-positional-arguments
+        self, hamiltonian, cost_layer, num_qaoa_layers, backend, initial_layout, optimized=False
+    ):
+        """Run both transpilation pipelines and compare expectation values
+        and number of final operations"""
+
+        param_values = [5.11350346] * num_qaoa_layers + [5.52673212] * num_qaoa_layers
+        standard_transpiled, annot_transpiled = self._run_standard_and_annot(
+            cost_layer, hamiltonian, num_qaoa_layers, backend, initial_layout, optimized
+        )
+        eval_standard = self._estimate(standard_transpiled, hamiltonian, param_values)
+        eval_annot = self._estimate(annot_transpiled, hamiltonian, param_values)
+
+        self._assert_equivalence(eval_annot, eval_standard, annot_transpiled, standard_transpiled)
+
     def _run_all_cases(self, problem_fn, optimized=False):
         """Iterate over given test cases and run"""
         for layers, nodes, edges, elist in self.test_cases:
             with self.subTest(layers=layers, nodes=nodes, edges=edges):
                 hamiltonian, cost_layer, _ = problem_fn(n=nodes, elist=elist)
                 backend, initial_layout = backend_and_layout_a(cost_layer)
-                self._run_comparison(
+                self._run_comparison_qopt(
                     hamiltonian, cost_layer, layers, backend, initial_layout, optimized
                 )
 
@@ -188,7 +237,7 @@ class TestAnnotatedTranspilation(unittest.TestCase):
             with self.subTest(layers=layers, nodes=nodes, edges=edges):
                 hamiltonian, cost_layer, _ = get_problem_barabasi(n=nodes, m=edges)
                 backend, initial_layout = backend_and_layout_a(cost_layer)
-                self._run_comparison(hamiltonian, cost_layer, layers, backend, initial_layout)
+                self._run_comparison_qopt(hamiltonian, cost_layer, layers, backend, initial_layout)
 
     def test_barabasi_albert_optimized(self):
         """Run comparison with barabasi albert graph and an additional synthesis/cancellation step."""
@@ -196,7 +245,7 @@ class TestAnnotatedTranspilation(unittest.TestCase):
             with self.subTest(layers=layers, nodes=nodes, edges=edges):
                 hamiltonian, cost_layer, _ = get_problem_barabasi(n=nodes, m=edges)
                 backend, initial_layout = backend_and_layout_a(cost_layer)
-                self._run_comparison(
+                self._run_comparison_qopt(
                     hamiltonian, cost_layer, layers, backend, initial_layout, optimized=True
                 )
 
@@ -207,6 +256,16 @@ class TestAnnotatedTranspilation(unittest.TestCase):
     def test_maxcut_optimized(self):
         """Run comparison with maxcut graph and an additional synthesis/cancellation step."""
         self._run_all_cases(get_problem_maxcut, optimized=True)
+
+    def test_standard(self):
+        """Run comparison against standard pipeline."""
+        hamiltonian = SparsePauliOp.from_list([("IZZ", 1), ("ZIZ", 2), ("ZZI", 3)])
+        cost_layer = get_cost_layer(hamiltonian)
+        backend, initial_layout = backend_and_layout_a(cost_layer)
+
+        self._run_comparison_standard(
+            hamiltonian, cost_layer, 1, backend, initial_layout, optimized=False
+        )
 
 
 if __name__ == "__main__":
